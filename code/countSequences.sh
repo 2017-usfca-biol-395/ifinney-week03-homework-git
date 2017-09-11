0e76e2498f4a1326196bcf88b76ec85724da0ff2
Bella Finney <ifinney@dons.usfca.edu>
2017-09-10 22:27:26 -0700

    Add final comments to countSequences script

--- code/countSequences.sh
+++ code/countSequences.sh
@@ -10,8 +10,13 @@
 cd ..
 cd data/raw_data/
 
+# This code sends the working directory to the repository and then into 
+# data and then finally into raw data
+
 for file in $@
 do
         echo "This file has $(grep -c "^>"  $file) sequences."
 done
 
+# This code outputs a script that reads each fasta file and reports 
+# sequence numbers
